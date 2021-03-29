2f28fcce0eeee0b66ddc8959442197b79b669189
Nurbol <azhimatovnurbol@gmail.com>
2021-03-29 19:18:42 +0600

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import "./style.css"
+
+// const age = 16
+// const name = "edil"
+
+// const app = (
+//     <h1 className = "title">
+//         My name is {name} I am {age}
+//     </h1>
+// )
+
+const App = () => {
+    return (
+        <div>
+        <Header/>
+        <Body/>
+        </div>
+    )
+}
+
+const Header = () => {
+    return(
+    <div id="myDIV" className="header">
+         <h2>My To Do List</h2>
+         <input type="text" id="myInput" placeholder="Title..."/>
+         <span className="addBtn">Add</span>
+    </div>
+    )
+}
+
+const Body = () => {
+    return (
+    <ul id="myUL">
+                <li>Hit the gym</li>
+                <li className="checked">Pay bills</li>
+                <li>Meet George</li>
+                <li>Buy eggs</li>
+                <li>Read a book</li>
+                <li>Organize office</li>
+    </ul>
+    )
+}
+
+// const Text = () => {
+//       return <p>Lorem, ipsum dolor sit amet consecte</p>
+// }
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App/>,
+    document.querySelector("#root")
+)
